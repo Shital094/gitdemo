061615a450b7d210cd1aaf17e6b2736dfd8e068b
HP <HP@LAPTOP-KNH49VFT>
2025-01-23 22:02:45 +0530

    calculator

---
+++ gitcalculator/src/gitcalculator/Clculator.java
@@ -0,0 +1,22 @@
+package gitcalculator;
+
+public class Clculator {
+
+	int x=80;
+	int y=20;
+	public void add() {
+		int c=x+y;
+		System.out.println("Addition is ="+c);
+	}
+	
+	public void sub() {
+		int c=x-y;
+		System.out.println("Substraction is ="+c);
+	}
+	
+	public void mul() {
+		int c=x*y;
+		System.out.println("multiplication is ="+c);
+	}
+	
+}
---
+++ gitcalculator/src/gitcalculator/Test.java
@@ -0,0 +1,10 @@
+package gitcalculator;
+
+public class Test {
+public static void main(String[] args) {
+	Clculator c= new Clculator();
+	c.add();
+	c.sub();
+	c.mul();
+}
+}
