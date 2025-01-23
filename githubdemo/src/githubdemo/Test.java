df493c849e241eb1662ca7fd0ba86e5d46d4e946
HP <HP@LAPTOP-KNH49VFT>
2025-01-23 17:02:00 +0530

    main method add

--- githubdemo/src/githubdemo/Test.java
+++ githubdemo/src/githubdemo/Test.java
@@ -2,10 +2,17 @@ package githubdemo;
 
 public class Test {
 
+		
 	public void m1() {
 		System.out.println("welcome github");
 	}
 	public void m2() {
 		System.out.println("welcome github");
 	}
+	public static void main(String[] args) {
+		Test t= new Test();
+		t.m1();
+		t.m2();
+	}
+	
 }
